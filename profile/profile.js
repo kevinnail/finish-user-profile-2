75957f0723c347a06afd75114163c1b903ba614e
kevinnail <fumalicious@gmail.com>
2022-09-27 15:33:44 -0700

    add update profile

--- profile/profile.js
+++ profile/profile.js
@@ -1,10 +1,12 @@
 // import '../auth/user.js';
 // > Part A: import updateProfile from fetch-utils.js
-import { updateProfile } from '../fetch-utils.js';
+// import { updateProfile } from '../fetch-utils.js';
 // > Part B: import getUser and getProfile from fetch-utils.js
+import { getProfile, updateProfile, getUser } from '../fetch-utils.js';
 
 // > Part B: get the user
-const user = null; // ???
+// const user = null; // ???
+const user = getUser();
 
 const errorDisplay = document.getElementById('error-display');
 const profileForm = document.getElementById('profile-form');
@@ -19,6 +21,9 @@ window.addEventListener('load', async () => {
     // > Part B:
     //      - get the profile based on user.id
     //      - set profile and error state from response object
+    const response = await getProfile(user.id);
+    error = response.error;
+    profile = response.data;
 
     if (error) {
         displayError();
@@ -71,6 +76,8 @@ profileForm.addEventListener('submit', async (e) => {
 
 function displayProfile() {
     // > Part B: update user name and bio from profile object
+    userNameInput.value = profile.user_name;
+    bioTextArea.value = profile.bio;
 }
 
 function displayError() {
